410eb127429112148543b024a8df347a4b2c142c
Shashank Bhat <38404074+shashankbhat2@users.noreply.github.com>
2020-12-28 06:16:56 +0530

    Update firebase.js

--- util/firebase.js
+++ util/firebase.js
@@ -14,8 +14,6 @@ export function loadDB() {
     };
     firebase.initializeApp(config);
   } catch (err) {
-    // we skip the "already exists" message which is
-    // not an actual error when we're hot-reloading
     if (!/already exists/.test(err.message)) {
       console.error('Firebase initialization error', err.stack);
     }
